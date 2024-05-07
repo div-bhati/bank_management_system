036befcd002312892b546993aa06ea29732c1323
Divyansh Bhati <87819841+div-bhati@users.noreply.github.com>
2024-05-07 20:18:50 +0530

    Update bank_management_project.sql
    Added transactions tables.

--- bank_management_project.sql
+++ bank_management_project.sql
@@ -2,25 +2,37 @@
 use bank_management_system;
 create table if not exists pan_records(
 	first_name varchar(20),
-    last_name varchar(20),
-    pan_number varchar(11) unique,
-    mobile_number varchar(10));
+    	last_name varchar(20),
+    	pan_number varchar(11) unique,
+    	mobile_number varchar(10));
 
 create table if not exists customer(
 	customer_id integer primary key,
 	first_name varchar(20) not null,
-    last_name varchar(20),
-    email varchar(30) not null unique,
-    phone_number varchar(20) unique ,
-    pan varchar(11) unique,
-    address varchar(100),
-    foreign key (pan) references pan_records(pan_number));
+   	last_name varchar(20),
+   	email varchar(30) not null unique,
+	phone_number varchar(20) unique ,
+	pan varchar(11) unique,
+	address varchar(100),
+	foreign key (pan) references pan_records(pan_number));
 
 create table if not exists accounts(
 	customer_id integer not null,
-    account_number integer primary key,
-    balance decimal(10,3),
-    foreign key (customer_id) references customer(customer_id));
+	account_number integer primary key,
+	balance decimal(10,3),
+	foreign key (customer_id) references customer(customer_id));
+
+create table transactions(
+	sender integer,
+	receiver integer,
+	amount varchar(50),
+	pan_record varchar(10),
+	phone_no varchar(20),
+	transaction_description varchar(500),
+	foreign key (sender) references accounts(account_number),
+	foreign key (receiver) references accounts(account_number),
+	foreign key (pan_record) references pan_records(pan_number),
+	foreign key (phone_no) references customer(phone_number));
 
 show tables;
 
